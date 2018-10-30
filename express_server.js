b2be06cfa19d51c6ef49d4325a9236555db002bc
Elbert <elbert.bae@gmail.com>
2018-10-30 23:31:25 +0000

    adding urls_index/show, working on _header partial

--- express_server.js
+++ express_server.js
@@ -4,17 +4,24 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
-app.get('/urls', (request, response) {
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-})
+app.get('/urls', (request, response) => {
+  let templateVariables = { urls: urlDatabase };
+  response.render('urls_index', templateVariables);
+});
 
-
-
-
-
-
+app.get('/urls/:id', (request, response) => {
+  let templateVariables = {
+    shortURL: request.params.id,
+    link: urlDatabase
+  };
+  response.render('urls_show', templateVariables);
+});
 
 // port verification on console
-app.listen(8080) {
-  console.log("Listening to port: 8080.");
-}
+app.listen(8080);
+console.log("Listening to port: 8080.");
