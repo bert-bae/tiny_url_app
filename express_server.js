9050ec8277989490bc9ad2293454f9b445c3deb1
Elbert <elbert.bae@gmail.com>
2018-10-30 20:43:00 +0000

    setting up tiny app project

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+
+app.get('/urls', (request, response) {
+
+})
+
+
+
+
+
+
+
+// port verification on console
+app.listen(8080) {
+  console.log("Listening to port: 8080.");
+}
