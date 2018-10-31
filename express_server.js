1179ac5a79e1b253a869e6db4770b4d937184056
Elbert <elbert.bae@gmail.com>
2018-10-31 19:47:44 +0000

    added update functionality for urls

--- express_server.js
+++ express_server.js
@@ -20,7 +20,8 @@ app.get('/urls', (request, response) => {
 app.post('/urls', (request, response) => {
   let newKey = generateRandomString();
   urlDatabase[newKey] = request.body.longURL;
-  response.send(`302: Redirecting to http://localhost:8080/urls/${newKey}`);
+  response.redirect('/urls');
+  // response.send(`302: Redirecting to http://localhost:8080/urls/${newKey}. </br> Your short link will go to: ${request.body.longURL}`);
 });
 
 app.get('/urls/new', (request, response) => {
@@ -33,6 +34,25 @@ app.get('/urls/:id', (request, response) => {
     link: urlDatabase
   };
   response.render('urls_show', templateVariables);
+});
+
+app.post('/urls/:id', (request, response) => {
+  let templateVariables = { urls: urlDatabase };
+  for (let link in urlDatabase) {
+    urlDatabase[link] = request.body.longURL;
+  }
+  console.log(urlDatabase);
+  response.redirect('/urls');
+});
+
+app.post('/urls/:id/delete', (request, response) => {
+  let templateVariables = { urls: urlDatabase };
+  for (let link in urlDatabase) {
+    if (link == request.params.id) {
+      delete urlDatabase[link];
+    }
+  }
+  response.redirect('/urls');
 });
 
 // https://expressjs.com/en/api.html (search req.params --> retrieving :shortURL)
